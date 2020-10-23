5f5881451c58f1cbc92ac2bc09c351d1499a12f0
SandKingg <47184464+SandKingg@users.noreply.github.com>
2020-10-23 15:07:50 +0100

    Brief Square class

--- ABC Logic/src/Square.java
+++ ABC Logic/src/Square.java
@@ -1,4 +1,44 @@
+import java.util.*;
 
 public class Square {
 
+	private char letter = '?';
+	private int row = -1;
+	private int col = -1;
+	private ArrayList<Character> possibilities = new ArrayList<Character>();
+	
+	public Square(int row, int col, ArrayList<Character> possibilities) {
+		this.row = row;
+		this.col = col;
+		this.possibilities = possibilities;
+	}
+	
+	public void checkPoss() {
+		if(possibilities.size() == 1) {
+			setLetter(possibilities.get(0));
+		}
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+	
+	public char getLetter() {
+		return letter;
+	}
+	
+	public ArrayList<Character> getPoss() {
+		return possibilities;
+	}
+	
+	public void setLetter(char l) {
+		if(letter == '?') {
+			letter = l;
+		}
+	}
+	
 }
